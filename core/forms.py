54671e5f48d79f31202af0f0cfbef31f43816980
Maciej Budzyński <maciej.budzynski@spoko.pl>
2018-07-17 12:57:14 +0200

    Changed to default save method in User model, fixed labels, added navbar to base, deleted unnecessary code.

--- core/forms.py
+++ core/forms.py
@@ -24,14 +24,9 @@ class UserCreateForm(forms.ModelForm):
             raise ValidationError(_("Password don't match"))
 
     def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            self.cleaned_data['email'],
-            self.cleaned_data['password'],
-            born_date=self.cleaned_data['born_date'],
-            sex=self.cleaned_data['sex'],
-            first_name=self.cleaned_data['first_name'],
-            last_name=self.cleaned_data['last_name'],
-            profile_pic=self.cleaned_data['profile_pic']
-        )
+        user = super(UserCreateForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
         return user
+
